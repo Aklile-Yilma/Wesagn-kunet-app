f0d91b331acbcadd93968b32ca9361401545265d
Aklile-Yilma <aklileyilma@gmail.com>
2022-05-28 08:28:43 +0300

    birth Serializers

--- Server/WesagnKunet/core/serializers.py
+++ Server/WesagnKunet/core/serializers.py
@@ -1,10 +1,9 @@
-from dataclasses import field
 from rest_framework import serializers
 from traitlets import validate
 
 from accounts.serializers import ClientSerializer
 from accounts.models import Client
-from .models import MarriageCertificate, CertificateDetails, Address
+from .models import BirthCertificate, MarriageCertificate, CertificateDetails, Address
 
 
 class DetailSerializer(serializers.ModelSerializer):
@@ -21,8 +20,6 @@ class AddressSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Address
 		fields = "__all__"
-
-
 
 class MarriageCertificateSerializer(serializers.ModelSerializer):
 
@@ -65,3 +62,62 @@ class MarriageCertificateSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = MarriageCertificate
 		fields = "__all__"
+
+
+class BirthCertificateSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model= BirthCertificate
+		fields= "__all__"
+	class childSerialzer(serializers.ModelSerializer):
+
+		class Meta:
+			model= BirthCertificate.child
+			fields= "__all__"
+
+	class parentSerialzer(serializers.ModelSerializer):
+
+		class Meta:
+			model= BirthCertificate.ParentInformation
+			fields= "__all__"
+
+	child= childSerialzer()
+	mother= parentSerialzer()
+	father= parentSerialzer()
+
+
+	def validate_child(self, child):
+		serializer = BirthCertificateSerializer.childSerialzer(data=child)
+		serializer.is_valid(raise_exception=True)
+		return serializer
+	
+	def validate_parent(self, mother):
+		serializer = BirthCertificateSerializer.parentSerialzer(data=mother)
+		serializer.is_valid(raise_exception=True)
+		return serializer
+
+	def validate_parent(self, father):
+		serializer = BirthCertificateSerializer.parentSerialzer(data=father)
+		serializer.is_valid(raise_exception=True)
+		return serializer
+
+	def create(self, validated_data):
+
+		detail = CertificateDetails()
+		detail.save()
+		detail.users.set([Client.objects.get(user=self.context["user"])])
+		instance = BirthCertificate(
+			child = validated_data.get("child").save(),
+			father = validated_data.get("father").save(),
+			mother = validated_data.get("father").save(),
+
+			detail = detail,
+			birth_date = validated_data["birth_date"]
+		)
+		instance.save()
+		return instance
+
+
+
+		
+		
