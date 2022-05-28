11380bff543b3a19d159b4aeecf9f6443ac2ee01
Aklile-Yilma <aklileyilma@gmail.com>
2022-05-28 08:06:10 +0300

    "Birth Certificate Model"

--- Server/WesagnKunet/core/admin.py
+++ Server/WesagnKunet/core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 
-from .models import MarriageCertificate, CertificateDetails, Country, Address
+from .models import MarriageCertificate, CertificateDetails, Country, Address, BirthCertificate
 
 admin.site.register(CertificateDetails)
 admin.site.register(MarriageCertificate.Spouse)
 admin.site.register(MarriageCertificate)
+admin.site.register(BirthCertificate)
 admin.site.register(Address)
 admin.site.register(Country)
