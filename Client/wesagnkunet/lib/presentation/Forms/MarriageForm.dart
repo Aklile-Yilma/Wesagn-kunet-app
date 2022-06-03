fa6db32cd9cc949ac8a05c61f6a8ae8d5e1ea73c
Aklile-Yilma <aklileyilma@gmail.com>
2022-06-03 03:03:40 +0300

    Validation for login page

---
+++ Client/wesagnkunet/lib/presentation/Forms/MarriageForm.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+
+class MarriageForm extends StatelessWidget {
+  MarriageForm({Key? key}) : super(key: key);
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    double _deviceWidth = MediaQuery.of(context).size.width;
+    double _deviceHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: SizedBox(
+          width: _deviceWidth * 0.6,
+          height: _deviceHeight * 0.8,
+          child: Stack(
+            children: [
+              _marriageInformation(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _marriageInformation(BuildContext context) {
+    return Form(
+      key: _formkey,
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        const Text(
+          "Marriage Information",
+          style: TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(0, 0, 139, 1),
+          ),
+        ),
+        _textField("Country of Marriage"),
+        _textField("City"),
+        _textField("Subcity"),
+        _numField("Wereda"),
+        _dateField("Date of Marriage"),
+        SizedBox(
+          width: 150,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: const Color.fromRGBO(0, 0, 139, 1),
+            ),
+            onPressed: () {
+              if (_formkey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Processing Data')),
+                );
+              }
+            },
+            child: const Text(
+              'Next',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget _wifeInformation(BuildContext context) {
+    return Form(
+      key: _formkey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Text(
+            "Wife's Information",
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(0, 0, 139, 1),
+            ),
+          ),
+          _textField("FirstName"),
+          _textField("Father Name"),
+          _textField("GrandFatherName"),
+          _textField("Nationaliry"),
+          _dateField("Date of Birth"),
+          _numField("Birth Certificate Id"),
+          _textField("Upload Photo"),
+          SizedBox(
+            width: 150,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: const Color.fromRGBO(0, 0, 139, 1),
+              ),
+              onPressed: () {
+                if (_formkey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text(
+                'Next',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _husbandInformation(BuildContext context) {
+    return Form(
+      key: _formkey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Text(
+            "Husband's Information",
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(0, 0, 139, 1),
+            ),
+          ),
+          _textField("FirstName"),
+          _textField("Father Name"),
+          _textField("GrandFatherName"),
+          _textField("Nationaliry"),
+          _dateField("Date of Birth"),
+          _numField("Birth Certificate Id"),
+          _textField("Upload Photo"),
+          SizedBox(
+            width: 150,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: const Color.fromRGBO(0, 0, 139, 1),
+              ),
+              onPressed: () {
+                if (_formkey.currentState!.validate()) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text(
+                'Submit',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _textField(String labelIn) {
+    return TextFormField(
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Please enter text";
+        }
+        if (double.tryParse(value) != null) {
+          return "Wrong Input";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          border: const OutlineInputBorder(), labelText: labelIn),
+    );
+  }
+
+  Widget _numField(String labelIn) {
+    return TextFormField(
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Please enter text";
+        }
+        return null;
+      },
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: labelIn,
+      ),
+    );
+  }
+
+  Widget _dateField(String label) {
+    return Column(
+      children: [
+        Text(label),
+        ElevatedButton(
+          child: const Text("Pick A Date"),
+          onPressed: () {
+            //   context: context,
+            //   intialDate: DateTime.now(),
+            //   firstDate: DateTime(1900),
+            //   lastDate: DateTime(2022),
+          },
+        ),
+      ],
+    );
+  }
+}
--- Client/wesagnkunet/lib/presentation/auth/Login.dart
+++ Client/wesagnkunet/lib/presentation/auth/Login.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  final _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -10,81 +13,96 @@ class LoginPage extends StatelessWidget {
     double _deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Center(
-        child: Container(
+        child: SizedBox(
           width: _deviceWidth * 0.5,
           height: _deviceHeight * 0.8,
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "LOGIN",
-                  style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(0, 0, 139, 1)),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'user@example.com',
-                    hintText: 'Enter your Email',
+            child: Form(
+              key: _formkey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "LOGIN",
+                    style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(0, 0, 139, 1)),
                   ),
-                ),
-                TextField(
-                    decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                )),
-                TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      foregroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromRGBO(255, 255, 255, 1)),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Color.fromRGBO(0, 0, 149, 1)),
-                      padding:
-                          MaterialStateProperty.all<EdgeInsets>(EdgeInsets.only(
-                        right: _deviceWidth * 0.05,
-                        left: _deviceWidth * 0.05,
-                        bottom: _deviceWidth * 0.05,
-                        top: _deviceWidth * 0.05,
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter email";
+                      }
+                      if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                        return "Please enter a valid email address";
+                      }
+                      return null;
+                    },
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'user@example.com',
+                      hintText: 'Enter your Email',
+                    ),
+                  ),
+                  TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter your password";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
                       )),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color.fromRGBO(0, 0, 139, 1),
                     ),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(fontSize: 15),
-                    )),
-                Center(
-                  child: RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "Don't have an account yet? ",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        TextSpan(
-                            text: 'Sign Up',
-                            style: TextStyle(color: Colors.blue),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                print("Sign Up clicked");
-                              }),
-                      ],
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                      }
+                    },
+                    child: const Text(
+                      'LogIn',
+                      style: TextStyle(fontSize: 20),
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(top: _deviceHeight * 0.1),
-                  child: Image.asset(
-                    "assets/images/logo-primary.png",
-                    width: _deviceWidth * 0.2,
-                    height: _deviceWidth * 0.2,
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: "Don't have an account yet? ",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          TextSpan(
+                              text: 'Sign Up',
+                              style: const TextStyle(color: Colors.blue),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  context.go("/Signup");
+                                }),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  Container(
+                    padding: EdgeInsets.only(top: _deviceHeight * 0.1),
+                    child: Image.asset(
+                      "assets/images/logo-primary.png",
+                      width: _deviceWidth * 0.2,
+                      height: _deviceWidth * 0.2,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
