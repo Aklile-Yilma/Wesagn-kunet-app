49b946eb9765a3cfbd17353a158295e461bbe883
temesgenzewude <temesgenzewude21@gmail.com>
2022-06-04 14:44:47 +0300

    implemented the admin panel

---
+++ Client/wesagnkunet/lib/presentation/admin/screens/sidemenu.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            DrawerListTile(
+              title: "Dashboard",
+              icon: Icon(Icons.dashboard),
+              press: () {
+                context.go("admin/board"); //TODO add the route in the main page
+              },
+            ),
+            DrawerListTile(
+              title: "Users",
+              icon: Icon(Icons.verified_user_sharp),
+              press: () {
+                context.go("admin/list_of_users");
+              },
+            ),
+            DrawerListTile(
+              title: "Certificates",
+              icon: Icon(Icons.folder),
+              press: () {
+                context.go("admin/");
+              },
+            ),
+            DrawerListTile(
+              title: "Notification",
+              icon: Icon(Icons.notifications),
+              press: () {
+                context.go("admin/"); //TODO the route
+              },
+            ),
+            DrawerListTile(
+              title: "Profile",
+              icon: Icon(Icons.account_box),
+              press: () {
+                context.go("admin/profile"); //TODO the route
+              },
+            ),
+            DrawerListTile(
+              title: "Settings",
+              icon: Icon(Icons.settings),
+              press: () {
+                context.go("admin/settings"); //TODO the route
+              },
+            ),
+          ],
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 250,
+              width: 400,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+                // color: Colors.white54,
+              ),
+              child: Image(
+                color: Colors.white,
+                width: 150,
+                height: 150,
+                image: AssetImage("assets/images/logo-primary.png"),
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile({
+    Key? key,
+    required this.title,
+    required this.icon,
+    required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final Icon icon;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+
+      // color: Colors.blueGrey,
+
+      child: ListTile(
+        onTap: press,
+        horizontalTitleGap: 0.0,
+        leading: icon,
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
