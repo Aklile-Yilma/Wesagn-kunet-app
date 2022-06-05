2571147b814992d0e132900dde40c0d6fc1f9af4
Aklile-Yilma <aklileyilma@gmail.com>
2022-06-05 19:03:38 +0300

    Birth form ui

--- Client/wesagnkunet/lib/main.dart
+++ Client/wesagnkunet/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:wesagnkunet/presentation/Forms/BirthForm.dart';
 import 'package:wesagnkunet/presentation/routes.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(MaterialApp(home: BirthForm()));
 }
 
 class MainApp extends StatelessWidget {
