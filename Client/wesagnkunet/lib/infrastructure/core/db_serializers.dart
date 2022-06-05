209c91aa13d709da55a042f521d104205e678033
Etsubdink Demeke <ETSUBDINKDE12@GMAIL.COM>
2022-06-05 14:49:29 +0300

    Create db_serializers.dart

---
+++ Client/wesagnkunet/lib/infrastructure/core/db_serializers.dart
@@ -0,0 +1,56 @@
+
+
+import 'package:wesagnkunet/domain/core/certificate.dart';
+import 'package:wesagnkunet/domain/core/marriage_certificate.dart';
+import 'package:wesagnkunet/infrastructure/lib/network/Serializer.dart';
+
+class MarriageCertificateDBSerializer extends Serializer<MarriageCertificate>{
+
+  @override
+  Map<String, dynamic> serialize(instance) {
+
+    String? issueDate = null;
+    if(instance.detail.issueDate != null){
+      issueDate = instance.detail.issueDate.toString();
+    }
+
+    return {
+      "id": instance.id,
+      "wife_first_name": instance.wife.firstName,
+      "wife_middle_name": instance.wife.middleName,
+      "wife_last_name": instance.wife.lastName,
+      "husband_first_name": instance.husband.firstName,
+      "husband_middle_name": instance.husband.middleName,
+      "husband_last_name": instance.husband.lastName,
+      "issue_date": issueDate,
+      "application_date": instance.detail.applicationDate.toString()
+    };
+
+  }
+  
+  @override
+  MarriageCertificate deSerialize(dynamic json) {
+
+    return MarriageCertificate(
+                        json["id"],
+                        Spouse(
+                          json["wife_first_name"],
+                          json["wife_middle_name"],
+                          json["wife_last_name"]
+                        ),
+                        Spouse(
+                          json["husband_first_name"],
+                          json["husband_middle_name"],
+                          json["husband_last_name"]
+                        ),
+                        CertificateDetail(
+                          [],
+                          DateTime.parse(json["issue_date"]),
+                          DateTime.parse(json["application_date"])
+                        ),
+                        DateTime.parse(json["marriage_date"]),
+                        false
+                      );
+
+  }
+}
